ca8aa373934c17fa64194cb2a2f2df6d1bdf85d3
DanilsonJsD <d97.dias@gmail.com>
2025-02-21 22:15:51 -0300

    Instalação novamente dos programas padrão e re-teste

--- meu -primeiro-projeto-java/src/App.java
+++ meu -primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem vinda Ane!");
+        System.out.println("Bem vinda Ane!!");
     }
 }
